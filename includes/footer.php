945de761e4e559ec6153e43b254cb4e72afe6291
leo <leobaranelli@gmail.com>
2024-04-10 16:35:37 -0300

    feat: footer file and main assets

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+            <div class="clearfix" ></div>
+        </div>
+        <footer id="footer">
+            <p>Developed by Leonardo Baranelli (based on and following a course by Víctor Robles)  &copy; 2024</p>
+        </footer>
+    </body>
+</html>
