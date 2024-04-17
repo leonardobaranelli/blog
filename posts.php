04a632344e36e3a084568c39a81feb8c4a1268a7
leo <leobaranelli@gmail.com>
2024-04-17 15:56:03 -0300

    feat: posts file

---
+++ posts.php
@@ -0,0 +1,27 @@
+<?php require_once "includes/header.php" ?>
+<?php require_once "includes/sidebar.php" ?>
+
+<!--MAIN BOX-->
+<div id="main">
+    <h1>All posts</h1>
+
+    <?php 
+        $posts = get_latest_posts();
+        if(!empty($posts)):
+            while($post = mysqli_fetch_assoc($posts)):
+                ?>
+                <article class="post">
+                    <a href="post.php?id=<?=$post['id']?>">
+                        <h2><?=$post['title']?></h2>
+                        <span class="date"><?=$post['category'].' | '.$post['date']?></span>
+                        <p>
+                            <?= substr($post['description'], 0, 180)."..." ?>
+                        </p>
+                    </a>    
+                </article>    
+                <?php
+            endwhile;
+        endif;
+    ?>    
+</div>
+<?php require_once "includes/footer.php" ?>
